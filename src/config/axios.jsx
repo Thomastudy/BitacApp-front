42b288f9229c2a39bcbbc49c181d5fd86a739843
Thomas <thomandoclases@gmail.com>
2025-04-30 15:15:49 -0300

    agregando variables de entorno para conectar con el backend de railway

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -11,8 +11,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
-  //import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
